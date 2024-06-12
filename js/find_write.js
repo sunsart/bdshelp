a195f869841e58b7426e2454f9da8db386c9f26d
sunsart <sunsart7@naver.com>
2024-06-12 16:42:36 +0900

    게시물 찾아요 테이블 아래 버튼 정렬 작업중

--- js/find_write.js
+++ js/find_write.js
@@ -33,20 +33,27 @@ function postFind() {
   }
 }
 
-//edit find
+// 매물찾아요 게시물 수정
 function editFind() {
-  let find_id = document.getElementById("find-id").value;
+  let find_id = document.getElementById("find_id").value;
   let title = document.getElementById("title").value;
   let region = document.getElementById("region").value;
   let area = document.getElementById("area").value;
   let cost = document.getElementById("cost").value;
   let tel = document.getElementById("tel").value;
   let etc = document.getElementById("etc").value;
+
+  // 전화번호 정규식, 000-0000-0000
+  const regexPhoneNum = /^[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}/;
   
   if(title == "")
     alert("제목을 입력하세요");
   else if(tel == "")
     alert("연락처를 입력하세요");
+  else if(regexPhoneNum.test(tel) === false)
+    alert("전화번호 형식이 올바르지 않습니다");
+  else if(etc == "")
+    alert("내용을 입력하세요");
   else {
     $.ajax({
       url : "/find_edit",
@@ -65,7 +72,7 @@ function editFind() {
 //delete find
 function deleteFind() {
   if(confirm("게시글을 삭제하시겠습니까?")) {
-    let find_id = document.getElementById("find-id").value;
+    let find_id = document.getElementById("find_id").value;
     $.ajax({
       url : "/find_delete",
       type : "POST",
